badc6bc6d8ef2650fcf16ab26bd6c1a1060e9fa2
Nikita Fiyas <97941401+fnicks@users.noreply.github.com>
2025-04-23 18:14:40 +0300

    parse env

--- Readme.md
+++ Readme.md
@@ -1,3 +1,6 @@
 # Node.js basics
 
 ## !!! Please don't submit Pull Requests to this repository !!!
+
+docs for this task:
+https://github.com/AlreadyBored/nodejs-assignments/blob/main/assignments/nodejs-basics/assignment.md
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    const rssEnv = [];
+    Object.entries(process.env).forEach(([key, value]) => {
+        if (key.startsWith('RSS')) rssEnv.push(`${key}=${value}`);
+    });
+    console.log(rssEnv.join('; '))
 };
 
 parseEnv();
