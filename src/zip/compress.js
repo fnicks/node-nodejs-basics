b04b9d98d6fe064b188df6cc0d295b06b289e972
Nikita Fiyas <97941401+fnicks@users.noreply.github.com>
2025-04-24 16:50:35 +0300

    zlib

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,13 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+
 const compress = async () => {
-    // Write your code here 
+    await pipeline(
+        createReadStream('src/zip/files/fileToCompress.txt'),
+        createGzip(),
+        createWriteStream('src/zip/files/archive.gz')
+    );
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,13 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+
 const decompress = async () => {
-    // Write your code here 
+    await pipeline(
+            createReadStream('src/zip/files/archive.gz'),
+            createGunzip(),
+            createWriteStream('src/zip/files/fileToCompress.txt')
+        );
 };
 
 await decompress();
