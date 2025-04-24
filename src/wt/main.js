ece3ee03e91c24125d1cd47daafa015915616c83
Nikita Fiyas <97941401+fnicks@users.noreply.github.com>
2025-04-24 17:22:56 +0300

    worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
 const performCalculations = async () => {
-    // Write your code here
+    const cpuCount = cpus().length;
+    const promises = [];
+
+    for (let i = 0; i < cpuCount; i++) {
+        const workerData = 10 + i;
+
+        const promise = new Promise((resolve) => {
+            const worker = new Worker(('./src/wt/worker.js'), {
+                workerData
+            });
+
+            worker.on('message', (data) => {
+                resolve({ status: 'resolved', data });
+            });
+
+            worker.on('error', () => {
+                resolve({ status: 'error', data: null });
+            });
+
+            worker.on('exit', (code) => {
+                if (code !== 0) {
+                    resolve({ status: 'error', data: null });
+                }
+            });
+        });
+
+        promises.push(promise);
+    }
+
+    const results = await Promise.all(promises);
+    console.log(results);
 };
 
 await performCalculations();
